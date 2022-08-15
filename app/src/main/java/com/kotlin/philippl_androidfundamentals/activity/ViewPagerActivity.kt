18294ad8c6a03ce178d022bb8c9ad090f02cad2b
yosuahaloho <halohoyosua1502@gmail.com>
2022-08-16 00:44:50 +0700

    Update new fundamental

--- app/src/main/java/com/kotlin/philippl_androidfundamentals/activity/ViewPagerActivity.kt
+++ app/src/main/java/com/kotlin/philippl_androidfundamentals/activity/ViewPagerActivity.kt
@@ -3,7 +3,6 @@ package com.kotlin.philippl_androidfundamentals.activity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.kotlin.philippl_androidfundamentals.R
